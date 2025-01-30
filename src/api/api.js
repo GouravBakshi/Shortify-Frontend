caf4b4e0c5026b9b6f29545326367c35a74de4d1
GouravBakshi <gouravbakshi1k1@gmail.com>
2025-01-30 17:36:14 +0530

    Handled Jwt Expiration

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,39 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+});
+
+// Request Interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("JWT_TOKEN")
+    ? JSON.parse(localStorage.getItem("JWT_TOKEN"))
+    : null;
+
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`; // Attach token in Authorization header
+    }
+    // If there is no token, simply return the config without modifications
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401 ) {
+      // Token expired or unauthorized - redirect to login page
+
+      localStorage.removeItem('JWT_TOKEN'); 
+      window.location.replace('/');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
